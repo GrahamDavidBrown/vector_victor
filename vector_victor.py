235d295d818dfa1d73739967700a7050c687eb8e
Graham Brown <GrahamDavidBrown90@gmail.com>
2017-02-28 13:35:00 -0500

    first commit

---
+++ vector_victor.py
@@ -0,0 +1,28 @@
+class Vector:
+    def __init__(self, start_point, end_point):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def shape():
+        pass
+
+    def vector_add():
+        pass
+
+    def vector_sub():
+        pass
+
+    def vector_sum():
+        pass
+
+    def dot():
+        pass
+
+    def vector_multiply():
+        pass
+
+    def vector_mean():
+        pass
+
+    def magnitude():
+        pass
