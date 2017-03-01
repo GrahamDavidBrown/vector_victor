2e51e2952382131e66c78f5db07f82aa68a6c3f0
Graham Brown <GrahamDavidBrown90@gmail.com>
2017-02-28 21:22:55 -0500

    some commit

--- vector_victor.py
+++ vector_victor.py
@@ -1,28 +1,73 @@
+class NotSameShapeError(ValueError):
+    pass
+
+
 class Vector:
-    def __init__(self, start_point, end_point):
-        self.start_point = start_point
-        self.end_point = end_point
+    def __init__(self, vector):
+        self.vector = vector
 
-    def shape():
-        pass
+    def shape(self):
+        return ((len(self.vector), ))
 
-    def vector_add():
-        pass
+    def __add__(self, other):
+        if self.shape != other.shape:
+            raise NotSameShapeError
+        else:
+            new_vector = Vector([])
+            zipped = zip(self.vector, other.vector)
+            new_vector.vector += [(pair[0] + pair[1]) for pair in zipped]
+            return new_vector
 
-    def vector_sub():
-        pass
+    def vector_add(self, other):
+        return(self + other)
 
-    def vector_sum():
-        pass
+    def __sub__(self, other):
+        if self.shape != other.shape:
+            raise NotSameShapeError
+        else:
+            new_vector = Vector([])
+            zipped = zip(self.vector, other.vector)
+            new_vector.vector += [(pair[0] - pair[1]) for pair in zipped]
+        return new_vector
 
-    def dot():
-        pass
+    def vector_sum(self, *args):
+        print(type(self))
+        new_vector = self
+        for arg in args:
+            new_vector += arg
+        return new_vector
 
-    def vector_multiply():
-        pass
+    def dot(self, other):
+        if self.shape != other.shape:
+            raise NotSameShapeError
+        else:
+            new_vector = Vector([])
+            zipped = zip(self.vector, other.vector)
+            new_vector.vector += [(pair[0] * pair[1]) for pair in zipped]
+            scalar = 0
+            for num in new_vector.vector:
+                scalar += num
+        return new_vector
 
-    def vector_mean():
-        pass
+    def vector_multiply(self, exponent):
+        new_vector = self
+        new_vector.vector = [num * exponent for num in new_vector.vector]
+        return new_vector
+
+    def vector_mean(self, *args):
+        new_vector = ([])
+        count = 1
+        count += [1 for arg in args]
+        new_vector.vector = new_vector.vector_add(*args)
+        new_vector.vector = new_vector.vector_multiply((1 / count))
 
     def magnitude():
         pass
+
+
+v1 = Vector([1, 2, 3, 4, 5])
+v2 = Vector([6, 7, 8, 9, 10])
+v3 = Vector([1, 1, 1, 1, 1])
+v6 = Vector([2, 2, 2, 2, 2])
+v5 = Vector([v1.vector_sum(v2, v3, v6)])
+v4 = ((v1 + v2 + v3))
